53626ea0ca3d4b1726b23f43eda86404fbc12eee
Agnieszka Pas <12623793+agapas@users.noreply.github.com>
2023-12-11 11:46:15 +0000

    Render remote-react app in isolation

---
+++ remote-react/src/bootstrap.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+// Mount function to start up the app
+const mount = (el) => {
+  const appRoot = createRoot(el);
+  appRoot.render(<h1>The Remote-react!</h1>);
+};
+
+// If in development and in isolation call mount immediately
+if (process.env.NODE_ENV === "development") {
+  const rootElement = document.getElementById("remote-react-root");
+  if (rootElement) {
+    mount(rootElement);
+  }
+}
+
+// Running through the host, so export the mount function
+export { mount };
