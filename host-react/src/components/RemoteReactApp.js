f97ad299fad58c28ecf52d7b9da93726dc225ac2
Agnieszka Pas <12623793+agapas@users.noreply.github.com>
2023-12-11 17:54:12 +0000

    Fix for useEffect async warning

--- host-react/src/components/RemoteReactApp.js
+++ host-react/src/components/RemoteReactApp.js
@@ -3,9 +3,23 @@ import React, { useRef, useEffect } from "react";
 const RemoteReactApp = () => {
   const ref = useRef(null);
 
-  useEffect(async () => {
-    const { mount } = await import("rr/RemoteReactApp");
-    mount(ref.current);
+  useEffect(() => {
+    /*
+      // using IFFE
+      (async () => {
+        try {
+          const { mount } = await import("rr/RemoteReactApp");
+          mount(ref.current);
+        } catch (err) {
+          console.log(err);
+        }
+      })();
+    */
+
+    // an old way, but looks more readable
+    import("rr/RemoteReactApp")
+      .then(({ mount }) => mount(ref.current))
+      .catch((err) => console.log(err));
   }, []);
 
   return <div ref={ref}></div>;
